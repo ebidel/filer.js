78f3e790d560235571323d355ed08a9bfd2bc5eb
Eric Bidelman <ebidel@gmail.com>
2013-03-12 20:32:45 -0700

    DOM4 Element.remove() collides with my remove() function

--- demos/js/app.js
+++ demos/js/app.js
@@ -119,7 +119,7 @@ function constructEntryHTML(entry, i) {
     html.push('<a href="', entry.toURL(), '" download><img src="images/icons/download.png" class="icon" title="Download" alt="Download"></a>');
   }
 
-  html.push('<a href="javascript:" data-remove-link onclick="remove(this,', i, ');"><img src="images/icons/trash_empty.png" class="icon" title="Remove" alt="Remove"></a>');
+  html.push('<a href="javascript:" data-remove-link onclick="removeEntry(this,', i, ');"><img src="images/icons/trash_empty.png" class="icon" title="Remove" alt="Remove"></a>');
 
   return html.join('');
 }
@@ -296,7 +296,7 @@ function rename(el, i) {
   toggleContentEditable(el);
 }
 
-function remove(link, i) {
+function removeEntry(link, i) {
   errors.textContent = ''; // Reset errors.
 
   var entry = entries[i];
